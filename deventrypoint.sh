497295da878ac137d34c8168c1da13dbd9b3ff15
maxhougas <max.hougas@gmail.com>
2025-02-23 22:43:53 +0000

    removed entry and exitpoint

--- deventrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /home/user/pcrpto/server
-node index.js &> /home/user/logsrv &
-cd /home/user/pcrpto/client
-npm run dev-https &> /home/user/logcli &
--- devexitpoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-kill $(ps -eo pid,comm | grep npm | grep -o '^ *[0-9]*') $(ps -eo pid,comm | grep node | grep -o '^ *[0-9]*')
